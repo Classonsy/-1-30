333eeb99a0ecd553aa951afc53d11ee0d47f692b
Classonsy <Classonsy@CLASSONSY>
2023-09-05 23:19:24 +0300

    Добавьте файлы проекта.

---
+++ Гвоздев задание 1-30/Program.cs
@@ -0,0 +1,576 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Гвоздев_задание_1_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 задание
+            try
+            {
+                Console.WriteLine("Add t");
+                double t = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add l");
+                double l = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"R={3 * Math.Pow(t, 3) + 3 * Math.Pow(l, 5) + 4.9}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 2 задание
+            try
+            {
+                Console.WriteLine("Add p");
+                double p = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"K={Math.Log10(Math.Pow(p, 2) + Math.Pow(y, 3)) + Math.Pow(e, p)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 3 задание
+            try
+            {
+                Console.WriteLine("Add n");
+                double n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"G={n * (y + 3.5) + Math.Sqrt(y)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 4 задание
+            try
+            {
+                Console.WriteLine("Add a");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add t");
+                double t = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"G={9.8 * Math.Pow(a, 2) + 5.52 * Math.Cos(Math.Pow(t, 5))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 5 задание
+            try
+            {
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"L={1.51 * Math.Cos(Math.Pow(x, 2) + 2 * Math.Pow(x, 3))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 6 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"M={Math.Cos(2 * y) + 3.6 * Math.Pow(e, x)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 7 задание
+            try
+            {
+                Console.WriteLine("Add m");
+                double m = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"N={Math.Pow(m, 2) + 2.8 * Math.Abs(m) + 0.55}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 8 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"T={6 * Math.Pow(y, 2 - 0.1 * y + 4)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 9 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"V={Math.Log10(y + 0.95) + Math.Sin(Math.Pow(x, 4))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 10 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add k");
+                double k = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"U={Math.Pow(e, x) + 7.355 * Math.Pow(k, 2) + Math.Pow(Math.Sin(x), 4)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 11 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add t");
+                double t = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add g");
+                double g = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"S={9.756 * Math.Pow(y, 7) + 2 * t * g * x}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 12 задание
+            try
+            {
+                Console.WriteLine("Add t");
+                double t = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"K={7 * Math.Pow(t, 2) + 3 * Math.Sin(Math.Pow(x, 3)) + 9.2}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 13 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"E={Math.Sqrt(Math.Abs(3 * Math.Pow(y, 2) + 0.5 * y + 4))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 14 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"R={Math.Abs(Math.Sqrt(Math.Pow(Math.Sin(y), 2 + 6.835) + Math.Pow(e, x)))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 15 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"H={Math.Sin(Math.Pow(y, 2)) - 2.8 * y + Math.Sqrt(Math.Abs(y))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 16 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"S={Math.Sqrt(Math.Cos(4 * Math.Pow(y, 2) + 7.151))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 17 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"N={3 * Math.Pow(y, 2) + Math.Sqrt(y + 1)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 18 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"N={3 * Math.Pow(y, 2) + Math.Sqrt(Math.Pow(y, 3) + 1)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 19 задание
+            try
+            {
+                Console.WriteLine("Add n");
+                double n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add g");
+                double g = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"P={n * Math.Sqrt(Math.Pow(y, 3) + 1.09 * g)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 20 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add k");
+                double k = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"U={Math.Pow(e, k + y) + Math.Tan(x) * Math.Sqrt(y)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 21 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add h");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"P={Math.Pow(e, y + 5.5) + 9.1 * Math.Pow(h, 3)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 22 задание
+            try
+            {
+                Console.WriteLine("Add u");
+                double u = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"T={Math.Sin(2 * u) * Math.Log10(2 * Math.Pow(y, 2) + Math.Sqrt(x))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 23 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add f");
+                double f = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"G={Math.Pow(e, 2 * y) + Math.Sin(f)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 24 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"F={Math.Sin(2) * 0.214 * Math.Pow(y, 5) + 1}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 25 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add f");
+                double f = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"G={Math.Pow(e, 2 * y) + Math.Sin(Math.Pow(f, 2))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 26 задание
+            try
+            {
+                Console.WriteLine("Add p");
+                double p = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Z={Math.Sin(Math.Pow(Math.Pow(p, 2) + 0.4, 3))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 27 задание
+            try
+            {
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add v");
+                double v = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add x");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"W={1.03 * v + Math.Pow(e, 2 * y) + Math.Tan(Math.Abs(x))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 28 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add h");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"T={Math.Pow(e, y + h) + Math.Sqrt(Math.Abs(6.4 * y))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 29 задание
+            try
+            {
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"N={3 * Math.Pow(y, 2) + Math.Sqrt(Math.Abs(y + 1))}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            #region 30 задание
+            try
+            {
+                Console.WriteLine("Add e");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add y");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Add r");
+                double r = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"W={Math.Pow(e, y + r) + 7.2 * Math.Sin(r)}");
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("NO!!!");
+            }
+            #endregion
+            Thread.Sleep(5000);
+        }
+    }
+}
